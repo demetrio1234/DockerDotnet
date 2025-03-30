4b9195ea5c9427afbe4a901f997d424fdfebc00e
demetrio1234 <dem.iaria@gmail.com>
2025-03-30 13:16:39 +0200

    Add DummyJsonData folder and some json dummy file Add some logic to get and show its content

--- jsonReader/Program.cs
+++ jsonReader/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var currentDirectory = Directory.GetCurrentDirectory();
+var folderName = "DummyJsonData";
+var path = Path.Combine([currentDirectory, folderName]);
+var files = Directory.EnumerateFileSystemEntries(path);
+
+Console.WriteLine("Here a list of all json files:");
+foreach (var file in files)
+    Console.WriteLine(file);
+
+// var storeJson = await File.ReadAllBytesAsync(File.Exists())
